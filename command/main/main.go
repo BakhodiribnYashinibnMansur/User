4fe5a5e3461099d2b2c94231266c5b4fa3d3ceba
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-03-12 09:38:03 +0500

    initial  handler

--- command/main/main.go
+++ command/main/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"users/internal/users"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+
+	router := gin.New()
+	handler := users.NewHandler()
+	handler.Register(router)
+	port := viper.GetString("server.port")
+	router.Run(":" + port)
+}
---
+++ internal/users/user.go
@@ -0,0 +1,46 @@
+package users
+
+import (
+	"users/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	usersURL = "/users"
+	userURL  = "/users/:uuid"
+)
+
+type handler struct{}
+
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
+func (h *handler) Register(router *gin.Engine) {
+	router.GET(usersURL, h.GetList)
+	router.GET(userURL, h.GetUserByID)
+	router.POST(usersURL, h.CreateUser)
+	router.PUT(userURL, h.UpdateUser)
+	router.PATCH(userURL, h.PartiallyUpdateUser)
+	router.DELETE(userURL, h.DeleteUser)
+}
+
+func (h *handler) GetList(ctx *gin.Context) {
+
+}
+func (h *handler) GetUserByID(ctx *gin.Context) {
+
+}
+func (h *handler) CreateUser(ctx *gin.Context) {
+
+}
+func (h *handler) UpdateUser(ctx *gin.Context) {
+
+}
+func (h *handler) PartiallyUpdateUser(ctx *gin.Context) {
+
+}
+func (h *handler) DeleteUser(ctx *gin.Context) {
+
+}
